e558cc4ae389b492d806a01da789aaba5f53d8b7
Chathula0X <konarashan@gmail.com>
2025-10-14 13:40:28 +0530

    make the supplier model and controller

---
+++ app/Http/Controllers/SupplierControllers.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SupplierControllers extends Controller
+{
+    public function index(){
+        $supplier = Supplier::all();
+        return view('AdminDashboard.Supplier.index', compact('suppliers'));
+        
+    }
+
+    public function create(){
+        return view('AdminDashboard.Supplier.create');
+
+    }
+
+    public function store(StoreSupplierRequest $request){
+        $supplier = Supplier::create($request->validated());
+        return redirect()->route('AdminDashboard.Supplier.index')->with('success','Supplier created successfully');
+    }
+
+    public function edit($supplier_id){
+        $supplier = Supplier::where('id', $supplier_id)->first();
+        return view('AdminDashboard.Supplier.edit', compact('supplier'));
+    }
+
+    public function update(UpdateSupplierRequest $request, $supplier_id){
+        $supplier = Supplier::where('id', $supplier_id)->first();
+        $supplier->update($request->validated());
+        return redirect()->route('AdminDashboard.Supplier.index')->with('success','Supplier updated successfully');
+    }
+
+    public function delete($supplier_id){
+        $supplier = Supplier::where('id', $supplier_id)->first();
+        $supplier->delete();
+        return redirect()->route('AdminDashboard.Supplier.index')->with('success','Supplier deleted successfully');
+
+    }
+}
